da9e4443f23d6ae0adca37ad42e8589a8c7be994
Ece Varol <ecevarol8@gmail.com>
2020-12-06 17:52:28 -0600

    bars finished

--- Assets/Scripts/BatteryBar.cs
+++ Assets/Scripts/BatteryBar.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Slider))]
 public class BatteryBar : MonoBehaviour
-{
+{   private Slider slider;
+    private Image fill;
+    private Text textBox;
+    public InformationManager iM;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
+        fill = slider.GetComponentsInChildren<Image>()[0];
+        textBox = slider.GetComponentsInChildren<Text>()[1];
+        SetMaxBatteryLevel(iM.GetMaxBatteryLevel());
+    }
+
+    public void SetMaxBatteryLevel (float maxLevel)
+    {
+        slider.maxValue = maxLevel;
+        slider.value = maxLevel; // Start with max!
+    }
+       public void SetBatteryLevel (float level)
+    {
+        slider.value = level;
+        textBox.text = ((int) level).ToString() + " %";
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetBatteryLevel(iM.GetBatteryLevel());
+
+        //if the oxygen level is less than 40 turn orange
+        if (slider.value < 40) {
+            fill.color = Color.yellow;
+
+        //if the oxygen level is less than 20 turn orange
+        } else if (slider.value < 20) {
+            fill.color = Color.red;
+
+        //default color is white
+        } else {
+            fill.color = Color.white;
+        }
         
     }
 }
