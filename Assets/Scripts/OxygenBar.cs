0c961d0095351c71460c7deccac0ba3c828c280a
Ece Varol <ecevarol8@gmail.com>
2020-12-06 17:20:20 -0600

    working on oxygen bar

--- Assets/Scripts/OxygenBar.cs
+++ Assets/Scripts/OxygenBar.cs
@@ -8,12 +8,14 @@ public class OxygenBar : MonoBehaviour
 {
 
     private Slider slider;
+    private Image fill;
     public InformationManager iM;
 
     // Start is called before the first frame update
     void Start()
     {
         slider = GetComponent<Slider>();
+        fill = slider.GetComponentsInChildren<Image>()[0];
         SetMaxOxygenLevel(iM.GetMaxOxygenLevel());
     }
 
@@ -31,5 +33,21 @@ public class OxygenBar : MonoBehaviour
     private void Update()
     {
         SetOxygenLevel(iM.GetOxygenLevel());
+
+        //if the oxygen level is less than 40 turn orange
+        if (slider.value < 40) {
+            fill.color = Color.yellow;
+
+        //if the oxygen level is less than 20 turn orange
+        } else if (slider.value < 20) {
+            fill.color = Color.red;
+
+        //default color is white
+        } else {
+            fill.color = Color.white;
+        }
+
     }
+
+
 }
