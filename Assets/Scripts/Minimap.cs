1362b5c745deaa56b5b1b4615c08d6be0f7ee8e6
spencerng <sng1488@gmail.com>
2020-12-07 19:54:44 -0500

    Add basic minimap UI

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+
+    private Transform backgroundTransform;
+    public Transform player;
+
+    private Vector3 northDirection = Vector3.zero;
+    private float prevAngle = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundTransform = GameObject.Find("Minimap Background").GetComponent<Transform>();
+        prevAngle = player.transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = - (prevAngle - player.transform.eulerAngles.y);
+        
+        backgroundTransform.Rotate(new Vector3(0f, 0f, rotation));
+        prevAngle = player.transform.eulerAngles.y;
+    }
+}
